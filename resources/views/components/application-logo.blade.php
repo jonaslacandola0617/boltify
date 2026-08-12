810b17784e8cf8b682ec95e9798068c1a8dde84c
Jonas Lacandola <jonaslacandola07@gmail.com>
2026-08-12 14:23:06 +0800

    refine Boltify brand mark

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,8 @@
-<h1 {{ $attributes->merge(['class' => 'font-black'])}}>
-    <span class="bg-gradient-to-r from-orange-500 to-orange-600 text-transparent bg-clip-text text-4xl">Boltify</span>
-</h1>
+<span {{ $attributes->merge(['class' => 'inline-flex items-center gap-2.5 text-zinc-950']) }}>
+    <span class="flex h-[1.65em] w-[1.65em] items-center justify-center rounded-[0.42em] bg-orange-500 shadow-sm shadow-orange-500/20">
+        <svg viewBox="0 0 32 32" class="h-[1em] w-[1em] fill-none stroke-zinc-950" aria-hidden="true">
+            <path d="M13 3 7.5 16H15l-2 13L24.5 13H17l2-10h-6Z" stroke-width="2.4" stroke-linejoin="round"/>
+        </svg>
+    </span>
+    <span class="font-extrabold tracking-[-0.055em]">Boltify</span>
+</span>
