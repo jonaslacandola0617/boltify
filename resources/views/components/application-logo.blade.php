7bd26432146c72602e9d9e4a6b70bc18b6b711d1
Jonas Lacandola <jonaslacandola07@gmail.com>
2026-08-12 15:21:43 +0800

    refine Boltify wordmark

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,8 +1,8 @@
 <span {{ $attributes->merge(['class' => 'inline-flex items-center gap-2.5 text-zinc-950']) }}>
-    <span class="flex h-[1.65em] w-[1.65em] items-center justify-center rounded-[0.42em] bg-orange-500 shadow-sm shadow-orange-500/20">
-        <svg viewBox="0 0 32 32" class="h-[1em] w-[1em] fill-none stroke-zinc-950" aria-hidden="true">
-            <path d="M13 3 7.5 16H15l-2 13L24.5 13H17l2-10h-6Z" stroke-width="2.4" stroke-linejoin="round"/>
+    <span class="flex h-[1.55em] w-[1.55em] items-center justify-center bg-orange-500">
+        <svg viewBox="0 0 32 32" class="h-[0.96em] w-[0.96em] fill-none stroke-zinc-950" aria-hidden="true">
+            <path d="M13 3 7.5 16H15l-2 13L24.5 13H17l2-10h-6Z" stroke-width="2.25" stroke-linejoin="round"/>
         </svg>
     </span>
-    <span class="font-extrabold tracking-[-0.055em]">Boltify</span>
+    <span class="font-extrabold tracking-[-0.06em]">Boltify</span>
 </span>
